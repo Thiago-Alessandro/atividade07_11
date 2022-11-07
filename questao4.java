4379cd5185b5a051ef607cb6d6dff9f4aa3ad2e1
Thiago-Alessandro <116910708+Thiago-Alessandro@users.noreply.github.com>
2022-11-07 16:56:14 -0300

    Create questao4.java

--- questao4.java
+++ questao4.java
@@ -1,10 +1,42 @@
 package atividade07_11;
 
+import java.util.Scanner;
+
 public class questao4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int[] numeros = new int[10];
+		int valorReferencia, vezesQueValorReferenciaAparece = 0, numerosMenoresValorReferencia = 0,
+				numerosMaioresvalorReferenciaExtensao = 0, contagemNumerosMaioresValorreferencia = 0;
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Insira um valor de referência inteiro: ");
+		valorReferencia = teclado.nextInt();
+		
+		for (int contador = 0;contador < 10;contador++) {
+			
+			System.out.print("Insira o valor para o0 número #" + contador + ": ");
+			numeros[contador] = teclado.nextInt();
+			
+			if (numeros[contador] < valorReferencia) {
+				numerosMenoresValorReferencia++;
+			}
+		}
+		
+		numerosMaioresvalorReferenciaExtensao = valorReferencia - numerosMenoresValorReferencia;
+		
+		 int[] numerosMaioresValorReferencia = new int[numerosMaioresvalorReferenciaExtensao];
+		 
+		 for (int contador = 0; contador < numerosMaioresValorReferencia.length;contador++) {
+			 
+			 if (numeros[contador] > valorReferencia) {
+				 
+				 numerosMaioresValorReferencia[contagemNumerosMaioresValorreferencia] = numeros[contador];
+			 }
+		 }
+		 System.out.print("Os números maiores que o valor referencial são: ");
 	}
 
 }
